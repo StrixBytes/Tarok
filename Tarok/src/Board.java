ed9e7421b45f973727979fe1a97e756d3526c1cf
Strix <bytes.strix@gmail.com>
2019-04-09 07:07:35 +0200

    Final logic commit
    This commit is all there is to logic and graphics changes, all that is
    left is a bit of  tidying up and documenting.

--- Tarok/src/Board.java
+++ Tarok/src/Board.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -7,6 +8,9 @@ import java.awt.FontMetrics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,7 +22,6 @@ import javax.swing.JPanel;
 //drawBotHands(g2);
 public class Board extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	private boolean inmenu;
 	private boolean ingame;
 	private boolean shuffled;
 	private boolean contractsRemoved;
@@ -33,7 +36,9 @@ public class Board extends JPanel implements ActionListener {
 	private GameLogic gl;
 	private Timer timer = new Timer(100, this);;
 	ImageLoader il = new ImageLoader();
-	private int cardWidth, LRplayerWidth, topPlayerWidth, startGameWidth, contractWidth, closeAnnWidth;
+	Font gameFont = new Font("Georgia", Font.ITALIC, ResolutionScaler.percentToHeight(2));
+	FontMetrics fm = getFontMetrics(gameFont);
+	private int cardWidth, LRplayerWidth, topPlayerWidth, buttonWidth,  menuButtonWidth;
 	private int talonSwap = 0;
 	private int selectedCard = -1, rSelectedCard = -1, tSelectedCard = -1, lSelectedCard = -1;
 	private int previousCard = -1, rPrevious = -1, tPrevious = -1, lPrevious = -1;
@@ -49,16 +54,14 @@ public class Board extends JPanel implements ActionListener {
 		addKeyListener(new ClickAdapter());
 		setFocusable(true);
 		setBackground(Color.BLACK);
-		inmenu = true;
 	}
 
 	private void setElementSize() {
 		cardWidth = ResolutionScaler.percentToWidth(6);
 		LRplayerWidth = ResolutionScaler.percentToWidth(12);
 		topPlayerWidth = ResolutionScaler.percentToWidth(6);
-		startGameWidth = ResolutionScaler.percentToWidth(20);
-		contractWidth = ResolutionScaler.percentToWidth(10);
-		closeAnnWidth = ResolutionScaler.percentToWidth(4);
+		buttonWidth = ResolutionScaler.percentToWidth(10);
+		menuButtonWidth=ResolutionScaler.percentToWidth(16);
 
 	}
 
@@ -105,23 +108,18 @@ public class Board extends JPanel implements ActionListener {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		if (il.isFlag(0)) {
-			inmenu = false;
-			ingame = true;
-			removeMenuBounds();
-			il.resetFlag(0);
-		}
+
 		removeAll();
-		if (inmenu) {
+		if (!ingame) {
 			setElementSize();
 			drawMenu(g);
-		} else if (ingame)
+		} else
 			drawGame(g);
 	}
 
 	private void drawGame(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-
+		g2.setFont(gameFont);
 		drawBackgound(g2, "Game");
 
 		if (!shuffled)
@@ -221,13 +219,22 @@ public class Board extends JPanel implements ActionListener {
 			URL imageUrl = this.getClass().getResource("/GameComponents/GameBackground.png");
 			ImageIcon ii = new ImageIcon(imageUrl);
 			temp = ii.getImage();
+		} else if (scene == "Menu") {
+			URL imageUrl = this.getClass().getResource("/MenuComponents/MenuBackground.png");
+			ImageIcon ii = new ImageIcon(imageUrl);
+			temp = ii.getImage();
 		}
 		g2.drawImage(temp, 0, 0, ResolutionScaler.getScreenWidth(), ResolutionScaler.getScreenHeight(), this);
 
 	}
 
 	private void replay(Graphics2D g2) {
-		drawClickable(g2, 49, "/MenuComponents/playButton.png", ResolutionScaler.percentToWidth(20), 40, 40);
+		drawClickable(g2, 14, "/GameComponents/SelectionBackground.png", ResolutionScaler.percentToWidth(12), 44, 45);
+		String replayMsg = "Konec Igre";
+		g2.setColor(Color.decode("#f4b942"));
+		g2.drawString(replayMsg, ResolutionScaler.percentToWidth(50) - fm.stringWidth(replayMsg) / 2,
+				ResolutionScaler.percentToHeight(48));
+		drawClickable(g2, 49, "/GameComponents/playAgain.png", buttonWidth, 45, 51);
 		if (il.isFlag(49)) {
 			shuffled = false;
 			contractsRemoved = false;
@@ -319,10 +326,7 @@ public class Board extends JPanel implements ActionListener {
 	private void showContracts(Graphics2D g2) {
 		drawClickable(g2, 14, "/GameComponents/SelectionBackground.png", ResolutionScaler.percentToWidth(32), 34, 35);
 		String contractMsg = "Izberi Igro";
-		Font contractFont = new Font("Georgia", Font.ITALIC, ResolutionScaler.percentToHeight(2));
-		FontMetrics fm = getFontMetrics(contractFont);
 		g2.setColor(Color.decode("#f4b942"));
-		g2.setFont(contractFont);
 		g2.drawString(contractMsg, ResolutionScaler.percentToWidth(50) - fm.stringWidth(contractMsg) / 2,
 				ResolutionScaler.percentToHeight(37));
 		int index = 0;
@@ -334,7 +338,7 @@ public class Board extends JPanel implements ActionListener {
 				"/GameComponents/contractButtonColorValat.png", "/GameComponents/contractButtonValat.png" };
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 3; j++) {
-				drawClickable(g2, 16 + index, pngName[index], contractWidth, 35 + j * 10, 38 + i * 6);
+				drawClickable(g2, 16 + index, pngName[index], buttonWidth, 35 + j * 10, 38 + i * 6);
 				index++;
 			}
 		}
@@ -366,7 +370,16 @@ public class Board extends JPanel implements ActionListener {
 			}
 		}
 		if (talonSwap < gl.talonSplitter()) {
-			drawClickable(g2, 34, "/GameComponents/button1.png", startGameWidth, 40, 30);
+			drawClickable(g2, 14, "/GameComponents/SelectionBackground.png", ResolutionScaler.percentToWidth(16), 42,
+					29);
+			String talonMsg1 = "Izberi del talona za menjavo";
+			String talonMsg2 = "in zamenjaj karte s svojimi";
+			g2.setColor(Color.decode("#f4b942"));
+			g2.drawString(talonMsg1, ResolutionScaler.percentToWidth(50) - fm.stringWidth(talonMsg1) / 2,
+					ResolutionScaler.percentToHeight(32));
+			g2.drawString(talonMsg2, ResolutionScaler.percentToWidth(50) - fm.stringWidth(talonMsg2) / 2,
+					ResolutionScaler.percentToHeight(35));
+			drawClickable(g2, 34, "/GameComponents/closeTalon.png", buttonWidth, 45, 37);
 			if (il.isFlag(34)) {
 				annReady = true;
 				repaint();
@@ -415,19 +428,24 @@ public class Board extends JPanel implements ActionListener {
 	}
 
 	private void showAnnouncements(Graphics2D g2) {
+		drawClickable(g2, 14, "/GameComponents/SelectionBackground.png", ResolutionScaler.percentToWidth(32), 34, 35);
+		String annMsg = "Izberi Napovedi";
+		g2.setColor(Color.decode("#f4b942"));
+		g2.drawString(annMsg, ResolutionScaler.percentToWidth(50) - fm.stringWidth(annMsg) / 2,
+				ResolutionScaler.percentToHeight(37));
 		int index = 0;
 		String[] pngName = { "/GameComponents/announceButtonKings.png", "/GameComponents/announceButtonKUltimo.png",
 				"/GameComponents/announceButtonPagat.png", "/GameComponents/announceButtonTrula.png",
 				"/GameComponents/announceButtonValat.png" };
 		for (int i = 0; i < 5; i++) {
 			if (i < 3)
-				drawClickable(g2, 39 + i, pngName[index], contractWidth, 30 + i * 15, 40);
+				drawClickable(g2, 39 + i, pngName[index], buttonWidth, 35 + i * 10, 40);
 			else
-				drawClickable(g2, 39 + i, pngName[index], contractWidth, 35 + (i - 3) * 20, 55);
+				drawClickable(g2, 39 + i, pngName[index], buttonWidth, 40 + (i - 3) * 10, 50);
 			index++;
 
 		}
-		drawClickable(g2, 44, "/GameComponents/closeAnnouncements.png", closeAnnWidth, 48, 25);
+		drawClickable(g2, 44, "/GameComponents/closeAnnouncements.png", buttonWidth, 45, 58);
 
 		gl.announcePicker();
 
@@ -520,10 +538,10 @@ public class Board extends JPanel implements ActionListener {
 
 		}
 
-		drawCard(g2, 45, selectedCard, 47, 46);
-		drawCard(g2, 46, rSelectedCard, 55, 34);
-		drawCard(g2, 47, tSelectedCard, 47, 21);
-		drawCard(g2, 48, lSelectedCard, 39, 34);
+		drawCard(g2, 45, selectedCard, 47, 52);
+		drawCard(g2, 46, rSelectedCard, 55, 40);
+		drawCard(g2, 47, tSelectedCard, 47, 27);
+		drawCard(g2, 48, lSelectedCard, 39, 40);
 		if (gl.isBeggarGame())
 			drawPrevious(g2);
 
@@ -555,7 +573,7 @@ public class Board extends JPanel implements ActionListener {
 		String tScore = "Zgornji Točke: " + topScore;
 		String rScore = "Desni Točke: " + rightScore;
 
-		g2.drawString(bScore, ResolutionScaler.percentToWidth(90), ResolutionScaler.percentToHeight(96));
+		g2.drawString(bScore, ResolutionScaler.percentToWidth(90), ResolutionScaler.percentToHeight(98));
 		g2.drawString(lScore, ResolutionScaler.percentToWidth(5) - fm.stringWidth(lScore) / 2,
 				ResolutionScaler.percentToHeight(2));
 		g2.drawString(tScore, ResolutionScaler.percentToWidth(50) - fm.stringWidth(tScore) / 2,
@@ -600,20 +618,35 @@ public class Board extends JPanel implements ActionListener {
 
 		// Top Player
 		for (int i = 0; i < gl.gettPlayerCards().size(); i++) {
-			drawClickable(g2, 15, "/GameComponents/cardBackgroundP.png", topPlayerWidth, 32 + 3 * i, 4);
+			drawClickable(g2, 15, "/GameComponents/cardBackgroundP.png", topPlayerWidth, 36 + 2 * i, 4);
 
 		}
 	}
 
 	private void drawMenu(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		setBackground(Color.decode("#6E8970"));
-		il.setImgIndex(0);
-		il.loadImage("/MenuComponents/playButton.png");
-		il.setDimensions(-1, ResolutionScaler.percentToWidth(40), ResolutionScaler.percentToHeight(40));
-		il.setCoordinates(-1, ResolutionScaler.percentToWidth(50) - il.getWidth(-1) / 2,
-				ResolutionScaler.percentToHeight(50) - il.getHeight(-1) / 2);
-		g2.drawImage(il.getImg(-1), il.getX(-1), il.getY(-1), il.getWidth(-1), il.getHeight(-1), this);
+		drawBackgound(g2, "Menu");
+
+			drawClickable(g2, 0, "/MenuComponents/startGameButton.png", menuButtonWidth, 42, 46);
+			drawClickable(g2, 1, "/MenuComponents/rulesButton.png", menuButtonWidth, 42, 52);
+		
+		if (il.isFlag(0)) {
+			ingame = true;
+			removeMenuBounds();
+			il.resetFlag(0);
+			repaint();
+		}else if (il.isFlag(1)) {
+			Desktop d = Desktop.getDesktop();
+			try {
+				d.browse(new URI("https://valat.si/pravila"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+			il.resetFlag(1);
+		}
+
 	}
 
 	private void removeMenuBounds() {
